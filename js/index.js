c2363324cdf5d22f8951e4db2554715c34c21b3d
Jonathan McCormick, Jr <67705789+McCormick9@users.noreply.github.com>
2021-01-13 09:41:55 -0600

    Create index.js
    Initial commit for index.js

---
+++ js/index.js
@@ -0,0 +1,12 @@
+//index.js
+//Jonathan McCormick, Jr. 
+//BlockchainProgrammer.tech
+//BlockchainProgrammer.crypto
+//Censorship is coming
+
+var dateOfUpdate = "13 Jan 2021"
+
+function CryptoMaintainanceAlert() {
+    alert("Welcome to BlockchainProgrammer.TECH, which you may browse at your convenience. Unfortunately, BlockchainProgrammer.CRYPTO is still being worked on. Please check back later if you want to see when it is ready ☺️");
+}
+CryptoMaintainanceAlert()
